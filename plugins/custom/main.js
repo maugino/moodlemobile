e4f2f03ab2fa5869ca8c830956c6506720d95fa1
Chris Maugino <maugino@gmail.com>
2015-04-21 13:27:59 +0100

    that's it

--- plugins/custom/main.js
+++ plugins/custom/main.js
@@ -1,23 +1,30 @@
 var templates = [
 "root/externallib/text!root/plugins/custom/theme.css",
 "root/externallib/text!root/plugins/custom/login.html",
-//"root/externallib/text!root/plugins/custom/program.html"
 ];
-// Inject allways our custom theme.
-$("#mobilecssurl").html(theme);
-// Replace the sign-up form with our custom template.
-$("#add-site_template").html(loginForm);
-// Automatically use the URL of the moodle moot without further checks.
-MM.checkSite = function(e) {
-MM.addSite(e);
-};
-// Inject allways our custom theme.
-MM.loadCachedRemoteCSS = function(e) {
-$("#mobilecssurl").html(theme);
-};
-// Do not display the manage accounts page.
-MM._displayManageAccounts = function() {
-MM._displayAddSite();
-};
-MM.registerPlugin(plugin);
+
+define(templates, function (theme, loginForm) {
+
+	// Inject allways our custom theme.
+	$("#mobilecssurl").html(theme);
+
+	// Replace the sign-up form with our custom template.
+	$("#add-site_template").html(loginForm);
+
+	// Automatically use the URL of the moodle moot without further checks.
+	MM.checkSite = function(e) {
+	MM.addSite(e);
+	};
+
+	// Inject allways our custom theme.
+	MM.loadCachedRemoteCSS = function(e) {
+	$("#mobilecssurl").html(theme);
+	};
+
+	// Do not display the manage accounts page.
+	MM._displayManageAccounts = function() {
+	MM._displayAddSite();
+	};
+
+	MM.registerPlugin(plugin);
 });
