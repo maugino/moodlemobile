69df2eca30a8f60b81905af80c1c176dd97c4c13
Chris Maugino <maugino@gmail.com>
2015-04-21 13:12:35 +0100

    fine tune again

--- plugins/custom/main.js
+++ plugins/custom/main.js
@@ -3,27 +3,6 @@ var templates = [
 "root/externallib/text!root/plugins/custom/login.html",
 //"root/externallib/text!root/plugins/custom/program.html"
 ];
-define(templates, function (theme, loginForm, program) {
-//var plugin = {
-//settings: {
-//name: "custom",
-//type: "general",
-//menuURL: "#custom",
-//icon: "plugins/events/icon.png",
-//lang: {
-//component: "core"
-//}
-},
-//routes: [
-//["custom", "show_program", "showProgram"]
-//["custom", "show_program", "showProgram"]
-//],
-//showProgram: function() {
-//var tpl = {};
-//var html = MM.tpl.render(program, tpl);
-MM.panels.show('center', html, {title: MM.lang.s("custom")});
-//}
-//};
 // Inject allways our custom theme.
 $("#mobilecssurl").html(theme);
 // Replace the sign-up form with our custom template.
